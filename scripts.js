1ac5526b4679c35acf2a534cb657708fb5b5df39
Abdul Razak <syedabdulrazak052000@gmail.com>
2024-12-14 00:07:54 +0530

    Add files via upload
    hamburger menu added

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
 // Example: Add interactivity for the movie images
 document.querySelectorAll('.movies img').forEach((img) => {
   img.addEventListener('click', () => {
       alert(`You clicked on ${img.alt}`);
   });
 });
+
